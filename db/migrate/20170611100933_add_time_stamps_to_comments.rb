aca0f5dfe128e39400d9fee3b1547ef27c537837
HaiLeader <pham.haisv@gmail.com>
2017-06-11 17:17:05 +0700

    add timestamps to comment table

---
+++ db/migrate/20170611100933_add_time_stamps_to_comments.rb
@@ -0,0 +1,6 @@
+class AddTimeStampsToComments < ActiveRecord::Migration[5.0]
+  def change
+    add_column :comments, :created_at, :datetime
+    add_column :comments, :updated_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609100859) do
+ActiveRecord::Schema.define(version: 20170611100933) do
 
   create_table "comments", force: :cascade do |t|
-    t.text    "content"
-    t.integer "user_id"
-    t.integer "post_id"
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
